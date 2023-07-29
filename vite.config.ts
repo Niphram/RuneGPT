7708120488abc5ef9c03c47e56fb67e093be7ba4
Niphram <theniphram@gmail.com>
2023-07-30 00:38:37 +0200

    Add plugin to support TopLevelAwait

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), topLevelAwait()],
 });
